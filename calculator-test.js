49e125ec1edb36b65ed24ee452b81fb317ad7adb
alocke8181 <alocke_8181@yahoo.com>
2023-03-29 18:45:29 -0400

    added more tests to the JS

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,30 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  const values = {amount : 10000, years : 10, rate: 2.5};
+  expect(calculateMonthlyPayment(values)).toEqual('94.27');
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  const values = {amount: 9971, years : 10, rate: 2.5};
+  expect(calculateMonthlyPayment(values)).toEqual('94.00');
+});
+
+it("should return zero", function(){
+  const values = {amount: 0, years : 10, rate: 2.5};
+  expect(calculateMonthlyPayment(values)).toEqual('0.00');
+});
+
+it("should return infinity", function(){
+  const values = {amount: 100000, years : 0, rate: 2.5};
+  expect(calculateMonthlyPayment(values)).toEqual('Infinity');
+});
+
+it("should return NaN", function(){
+  const values = {amount: 100000, years : 10, rate: 0};
+  expect(calculateMonthlyPayment(values)).toEqual('NaN');
 });
 
 /// etc
